67af504648b746b6a4588f7bc656509166ea90db
Moataz0000 <moatazfawzy150@gmail.com>
2025-12-20 20:32:32 +0200

    feat: updates

--- config/apps/users/models/admin.py
+++ config/apps/users/models/admin.py
@@ -6,6 +6,7 @@ from .user import User
 
 class AdminUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=150, unique=True, verbose_name=_("Username"))
     image = models.ImageField(
         upload_to="admin_users/images/", blank=True, null=True, verbose_name=_("Image")
     )
@@ -21,4 +22,10 @@ class AdminUser(models.Model):
         if not self.user.is_staff:
             self.user.is_staff = True
             self.user.save(update_fields=["is_staff"])
+        if not self.username:
+            self.username = self.user.email.split("@")[0]
         super().save(*args, **kwargs)
+
+    @property
+    def is_active(self):
+        return self.user.is_active
