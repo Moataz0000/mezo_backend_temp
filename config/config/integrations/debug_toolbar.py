a9f66a908348dbe16e7feccc8df61923db84fecf
Moataz0000 <moatazfawzy150@gmail.com>
2025-12-21 01:35:21 +0200

    feat: updates

--- config/config/integrations/debug_toolbar.py
+++ config/config/integrations/debug_toolbar.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
 DEBUG_TOOLBAR_CONFIG = {
-    "SHOW_TOOLBAR_CALLBACK": lambda request: DEBUG,
+    "SHOW_TOOLBAR_CALLBACK": lambda request: settings.DEBUG,
 }
