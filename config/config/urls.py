bfd507f828fbde0bf015d813f8f896c4499c7d19
Moataz0000 <moatazfawzy150@gmail.com>
2025-12-21 01:14:43 +0200

    feat: updates

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,10 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import URLPattern, URLResolver, include, path
+
+api_app_patterns: list[URLPattern | URLResolver] = [
+    path("users/", include("apps.users.api.root.urls")),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(api_app_patterns)),
 ]
 
 if settings.DEBUG:
